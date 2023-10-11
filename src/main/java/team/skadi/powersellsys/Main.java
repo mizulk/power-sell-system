c5796979f6cc4855abe724c03497cfe3bccc0276
mizulk <1930764560@qq.com>
2023-10-12 01:34:03 +0800

    将mybatis-config.xml添加到gitignore，新增加密码适配功能

--- src/main/java/team/skadi/powersellsys/Main.java
+++ src/main/java/team/skadi/powersellsys/Main.java
@@ -1,9 +1,16 @@
 package team.skadi.powersellsys;
 
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.plaf.FontUIResource;
 import java.awt.Font;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Enumeration;
 
 public class Main {
@@ -14,6 +21,32 @@ public class Main {
 	public static final Font MIDDLE_FONT = new Font(FONT_NAME, Font.PLAIN, 28);
 	public static final Font TITLE_FONT = new Font(FONT_NAME, Font.PLAIN, 42);
 	public static final Font TINY_FONT = new Font(FONT_NAME, Font.PLAIN, 14);
+
+	static {
+		String configPath = "src/main/resources/mybatis-config.xml";
+		String templatePath = "src/main/resources/mybatis-config-template.xml";
+		File file = new File(configPath);
+		if (!file.exists()) {
+			try {
+				String password = JOptionPane.showInputDialog("检测到无mybatis配置文件\n请输入你的MySQL密码来生成配置文件：", "123456");
+				BufferedReader in = new BufferedReader(new FileReader(templatePath));
+				BufferedWriter out = new BufferedWriter(new FileWriter(file));
+				String line;
+				while ((line = in.readLine()) != null) {
+					if (line.contains("password"))
+						out.write("\t\t\t\t<property name=\"password\" value=\"" + password + "\"/>\n");
+					else
+						out.write(line + "\n");
+				}
+				out.flush();
+				out.close();
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(null, "生成配置文件失败: " + e);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		// 设置全局字体
